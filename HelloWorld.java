d5b184413802a576062f0dea6bb9e1a769900b1b
Omarceloo <omarcello@ciencias.unam.mx>
2026-02-16 19:30:25 -0600

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class HelloWorld {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello World");
+
+        System.out.print("Introduce tu nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Introduce tu edad: ");
+        int edad = sc.nextInt();
+
+        System.out.print("Introduce tu altura (decimal): ");
+        double altura = sc.nextDouble();
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        sc.close();
     }
+
 }
